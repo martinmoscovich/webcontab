37f24442bdff0e1c5559cf8087acf3f089be319c
Martin Moscovich <martin.moscovich@pjn.gov.ar>
2020-09-17 19:58:06 -0300

    [Client] Se ordenan las subcuentas por numero en la lista

--- client/src/components/categorias/Subcuentas.vue
+++ client/src/components/categorias/Subcuentas.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="panel" v-if="categoria.children">
-    <a class="panel-block" v-for="item in categoria.children" :key="item.id" @click="onItemClick(item)">
+    <a class="panel-block" v-for="item in items" :key="item.id" @click="onItemClick(item)">
       <span class="panel-icon">
         <b-icon :icon="getIcon(item)" custom-size="mdi-24px" size="is-small" />
       </span>
@@ -11,6 +11,7 @@
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
 import { Cuenta, Categoria, CuentaOCategoria } from '@/model/Cuenta';
+import { buildCompareFn } from '@/utils/array';
 
 /**
  * Lista de subcategorias / subcuentas de una Categoria
@@ -23,6 +24,11 @@ export default class Subcuentas extends Vue {
   /** Icono a mostrar en el item */
   private getIcon(item: Cuenta | Categoria) {
     return item.imputable ? 'file' : 'folder';
+  }
+
+  /** Subcuentas ordenadas por numero */
+  private get items() {
+    return this.categoria.children?.sort(buildCompareFn({ field: 'numero' })) ?? null;
   }
 
   /** Handler cuando se selecciona un hijo */
