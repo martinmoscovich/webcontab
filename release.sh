967f575990feaf3674f5ee8c33c3c71d18f0205f
Martin Moscovich <martin.moscovich@pjn.gov.ar>
2020-12-14 11:11:42 -0300

    Mejoras en el script para incrementar version

--- release.sh
+++ release.sh
@@ -1,10 +1,9 @@
-if [ -z $1 ]; then
-    echo "Defina la nueva version"
-    exit 1;
-fi
+# Busca la nueva version del server
+VERSION=$(mvn help:evaluate -Dexpression=server.version -q -DforceStdout)
+NEXT_VERSION=$((VERSION + 1))
 
 # Incrementa la version del server
-mvn versions:set-property -DgenerateBackupPoms=false -Dproperty=server.version -DnewVersion=$1
+mvn versions:set-property -DgenerateBackupPoms=false -Dproperty=server.version -DnewVersion=$NEXT_VERSION
 
 # Incrementa la version de la aplicacion
 mvn build-helper:parse-version versions:set -DgenerateBackupPoms=false -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}\${parsedVersion.qualifier?}
